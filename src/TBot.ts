60c76f65296be9e00c6965694d260928fb00fd4a
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-06-17 16:39:02 +0200

    adds message event handler

--- src/TBot.ts
+++ src/TBot.ts
@@ -1,18 +1,84 @@
-import TelegramBot, { Message, Metadata } from 'node-telegram-bot-api';
+import TelegramBot, {
+  Message,
+  MessageType,
+  Metadata,
+  SendMessageOptions,
+} from 'node-telegram-bot-api';
 
 /**
  * Simple Telegram bot api wrapper
  */
 export default class TBot {
-	bot: TelegramBot;
-	/**
-	 * Constructor for the TelegramBot
-	 * 
-	 * @param token Auth Token from Telegram
-	 */
-	constructor(token: string) {
-		this.bot = new TelegramBot(token, {polling: true})
-		this.bot.on('message', (message: Message, metadata: Metadata) => console.log("Jona", message, metadata));
-	}
+  private bot: TelegramBot;
+  private commands: Command[];
 
+  /**
+   * Constructor for the TelegramBot
+   *
+   * @param token Auth Token from Telegram
+   */
+  constructor(token: string) {
+    this.bot = new TelegramBot(token, { polling: true });
+    this.commands = [];
+
+    this.addEventListeners();
+  }
+
+  private addEventListeners() {
+    this.bot.on('message', (message: Message, metadata: Metadata) =>
+      this.onMessage(message, metadata)
+    );
+  }
+
+  private onMessage(message: Message, metadata: Metadata) {
+    this.commands.forEach(({ name, callback }) => {
+      let text = message.text;
+      if (text && new RegExp(`^\/${name}(\ |$)`).test(text)) {
+        const params = text.replace(new RegExp(`^\/${name}\(\ |$)`), '');
+        let parsedParams = params.split(/\s+/);
+        if (parsedParams[0] === '') parsedParams = [];
+        callback(new Context(this.bot, message, metadata), ...parsedParams);
+      }
+    });
+  }
+
+  public addCommand(
+    command: string,
+    callback: (context: Context, ...params: string[]) => void
+  ) {
+    this.commands.push({ name: command, callback });
+  }
+}
+
+export type Command = {
+  /**
+   * Name of the command
+   */
+  name: string;
+  /**
+   * Callback function for the command
+   */
+  callback: (context: Context, ...params: string[]) => void;
+};
+
+export class Context {
+  private bot: TelegramBot;
+  message: Message;
+  type?: MessageType;
+
+  constructor(bot: TelegramBot, message: Message, metadata: Metadata) {
+    this.bot = bot;
+    this.message = message;
+    this.type = metadata.type;
+  }
+
+  /**
+   *
+   * @param content Content of the message
+   * @param options Options
+   * @returns
+   */
+  reply(content: string, options?: SendMessageOptions | undefined) {
+    return this.bot.sendMessage(this.message.chat.id, content, options);
+  }
 }
--- src/example.ts
+++ src/example.ts
@@ -1,7 +1,13 @@
-import TBot from './TBot'
+import TBot, { Context } from './TBot';
 
-import dotenv from 'dotenv'
-dotenv.config()
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 const bot = new TBot(process.env.TELEGRAM_BOT_API_TOKEN as string);
+
+bot.addCommand('test', async (c: Context, ...params) => {
+  console.log(params);
+  const message = await c.reply('Hey');
+
+  console.log(message);
+});
